069468eb3dcd47478d1b131e122fe21c52d40c49
Bahruz Shabili <bahruz.dsh@code.edu.az>
2024-04-30 20:56:58 +0400

    DAO V 2.0 implemented

--- src/main/java/az/edu/turing/stepProjBookingApp/dao/impl/FlightFileDao.java
+++ src/main/java/az/edu/turing/stepProjBookingApp/dao/impl/FlightFileDao.java
@@ -2,6 +2,7 @@ package az.edu.turing.stepProjBookingApp.dao.impl;
 
 import az.edu.turing.stepProjBookingApp.dao.FlightDao;
 import az.edu.turing.stepProjBookingApp.model.entity.FlightEntity;
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 
@@ -9,13 +10,17 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 import java.util.Optional;
 import java.util.function.Predicate;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class FlightFileDao extends FlightDao {
     private static final String RESOURCE_PATH = "src/main/java/az/edu/turing/stepProjBookingApp/resource";
-    private static final String FLIGHT_FILE_PATH = RESOURCE_PATH.concat("flights.txt");
+    private static final String FLIGHT_FILE_PATH = RESOURCE_PATH.concat("flights.json");
     private final ObjectMapper objectMapper;
 
     public FlightFileDao(ObjectMapper objectMapper) {
@@ -23,35 +28,71 @@ public class FlightFileDao extends FlightDao {
     }
 
     @Override
-    public FlightEntity save (FlightEntity flightEntity){
+    public boolean reservFlight(FlightEntity flightEntity) {
         try {
-            final Path path = Paths.get(FLIGHT_FILE_PATH);
-            Files.write(path, objectMapper.writeValueAsBytes(flightEntity));
-        } catch (IOException e){
-            System.err.println("Error while saving flight to file ...");
+            byte[] jsonData = Files.readAllBytes(Paths.get(FLIGHT_FILE_PATH));
+            List<FlightEntity> flights = objectMapper.readValue(jsonData, new TypeReference<List<FlightEntity>>() {});
+            flights.add(flightEntity);
+            Files.write(Paths.get(FLIGHT_FILE_PATH), objectMapper.writeValueAsBytes(flights));
+            return true;
+        } catch (IOException e) {
+            System.err.println("Error while adding new flight: " + e.getMessage());
         }
-        return flightEntity;
+        return false;
     }
 
     @Override
-    public Collection<FlightEntity> getAllFlights (){
-        final Path path = Paths.get(FLIGHT_FILE_PATH);
-
+    public Collection<FlightEntity> getAllFlights() {
+        try {
+            byte[] jsonData = Files.readAllBytes(Paths.get(FLIGHT_FILE_PATH));
+            return objectMapper.readValue(jsonData, new TypeReference<Collection<FlightEntity>>() {
+            });
+        } catch (Exception e) {
+            System.out.println("Error while loading flights from file: " + e.getMessage());
+        }
         return null;
     }
 
     @Override
     public Optional<FlightEntity> getFlightBy(Predicate<FlightEntity> predicate) {
+        try {
+            byte[] jsonData = Files.readAllBytes(Paths.get(FLIGHT_FILE_PATH));
+            FlightEntity[] flights = objectMapper.readValue(jsonData, FlightEntity[].class);
+            return Stream.of(flights)
+                    .filter(predicate)
+                    .findFirst();
+        } catch (IOException e) {
+            System.out.println("Error while reading flights from file: " + e.getMessage());
+        }
         return Optional.empty();
     }
 
     @Override
     public Collection<FlightEntity> getFlightsBy(Predicate<FlightEntity> predicate) {
-        return null;
+        try {
+            byte[] jsonData = Files.readAllBytes(Paths.get(FLIGHT_FILE_PATH));
+            FlightEntity[] flights = objectMapper.readValue(jsonData, FlightEntity[].class);
+            return Stream.of(flights)
+                    .filter(predicate)
+                    .toList();
+        } catch (IOException e) {
+            System.out.println("Error while reading flights from file: " + e.getMessage());
+        }
+        return new ArrayList<>();
     }
 
     @Override
-    public void update(FlightEntity flightEntity) {
-
+    public boolean cancelFlightBy(Predicate <FlightEntity> predicate) {
+        try {
+            byte [] jsonData = Files.readAllBytes(Paths.get(FLIGHT_FILE_PATH));
+            List<FlightEntity> flights = objectMapper.readValue(jsonData, new TypeReference<List<FlightEntity>>() {});
+            Optional<FlightEntity> flightForRemove = flights.stream().filter(predicate).findFirst();
+            flights.remove(flightForRemove);
+            Files.write(Paths.get(FLIGHT_FILE_PATH), objectMapper.writeValueAsBytes(flights));
+            return true;
+        }catch (Exception e){
+            System.out.println("Error occured while removing reservation: " + e.getMessage());
+        }
+        return false;
     }
 }
