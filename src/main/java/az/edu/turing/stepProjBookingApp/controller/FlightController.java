c88de8d23e9592dea124823f9c4bb183bcaf5c12
Heybat <heybetmovlamov2005@gmail.com>
2024-05-01 14:23:29 +0100

    Flight Service new

--- src/main/java/az/edu/turing/stepProjBookingApp/controller/FlightController.java
+++ src/main/java/az/edu/turing/stepProjBookingApp/controller/FlightController.java
@@ -1,29 +1,20 @@
-//package az.edu.turing.stepProjBookingApp.controller;
-//
-//import az.edu.turing.stepProjBookingApp.model.dto.FlightDto;
-//import az.edu.turing.stepProjBookingApp.model.entity.FlightEntity;
-//import az.edu.turing.stepProjBookingApp.service.FlightService;
-//
-//import java.util.List;
-//import java.util.Optional;
-//
-//public class FlightController {
-//
-//    public FlightService flightService;
-//
-//    public List<FlightEntity> onlineBoard() {
-//        return flightService.getAllFlights();
-//    }
-//
-//    public boolean reservation(FlightDto flightDto) {
-//        return flightService.reservation(flightDto);
-//    }
-//
-//    public boolean cancelReservation(long id) {
-//        return flightService.cancelReservation(id);
-//    }
-//
-//    public Optional<FlightEntity> getFlightById (long id){
-//        return flightService.getFlightById(id);
-//    }
-//}
+package az.edu.turing.stepProjBookingApp.controller;
+
+import az.edu.turing.stepProjBookingApp.model.dto.FlightDto;
+import az.edu.turing.stepProjBookingApp.model.entity.FlightEntity;
+import az.edu.turing.stepProjBookingApp.service.FlightService;
+
+import java.util.List;
+import java.util.Optional;
+
+public class FlightController {
+
+    public FlightService flightService;
+
+    public List<FlightDto> onlineBoard() {
+        return flightService.getAllFlights();
+    }
+    public Optional<FlightDto> getFlightById (long id){
+        return flightService.getFlightById(id);
+    }
+}
--- src/main/java/az/edu/turing/stepProjBookingApp/service/FlightService.java
+++ src/main/java/az/edu/turing/stepProjBookingApp/service/FlightService.java
@@ -1,18 +1,15 @@
-//package az.edu.turing.stepProjBookingApp.service;
-//
-//import az.edu.turing.stepProjBookingApp.model.dto.FlightDto;
-//import az.edu.turing.stepProjBookingApp.model.entity.FlightEntity;
-//
-//import java.util.List;
-//import java.util.Optional;
-//
-//public interface FlightService {
-//
-//    Boolean reservation(FlightDto flightDto);
-//
-//    boolean cancelReservation(long id);
-//
-//    List<FlightEntity> getAllFlights();
-//
-//    Optional<FlightEntity> getFlightById(long id);
-//}
+package az.edu.turing.stepProjBookingApp.service;
+
+import az.edu.turing.stepProjBookingApp.model.dto.FlightDto;
+import az.edu.turing.stepProjBookingApp.model.entity.FlightEntity;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface FlightService {
+    List<FlightDto> getAllFlights();
+
+    List<FlightDto> getAllByDest(String destination);
+
+    Optional<FlightDto> getFlightById(long id);
+}
