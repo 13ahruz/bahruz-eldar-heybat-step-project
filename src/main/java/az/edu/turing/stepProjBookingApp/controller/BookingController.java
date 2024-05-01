9a685602a7d16366153bb89310594d8e6b75e448
Bahruz Shabili <bahruz.dsh@code.edu.az>
2024-05-01 17:40:07 +0400

    Booking controller implemented

--- src/main/java/az/edu/turing/stepProjBookingApp/controller/BookingController.java
+++ src/main/java/az/edu/turing/stepProjBookingApp/controller/BookingController.java
@@ -1,9 +1,23 @@
 package az.edu.turing.stepProjBookingApp.controller;
 
+import az.edu.turing.stepProjBookingApp.model.dto.BookingDto;
 import az.edu.turing.stepProjBookingApp.service.BookingService;
+
+import java.util.Collection;
 
 public class BookingController {
     private BookingService bookingService;
 
+    public boolean bookAReservation(String firstName, String secondName) {
+        return bookingService.bookAReservation(firstName, secondName);
+    }
+
+    public boolean cancelAReservation(long id) {
+        return bookingService.cancelAReservation(id);
+    }
+
+    public Collection<BookingDto> getMyReservations(String firstName, String secondName) {
+        return bookingService.getMyReservations(firstName, secondName);
+    }
 
 }
