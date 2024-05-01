498f71aec1e745419a454c7f4f04fc4564eff878
Bahruz Shabili <bahruz.dsh@code.edu.az>
2024-05-01 15:56:14 +0400

    New flight DAO

--- src/main/java/az/edu/turing/stepProjBookingApp/controller/FlightController.java
+++ src/main/java/az/edu/turing/stepProjBookingApp/controller/FlightController.java
@@ -1,29 +1,29 @@
-package az.edu.turing.stepProjBookingApp.controller;
-
-import az.edu.turing.stepProjBookingApp.model.dto.FlightDto;
-import az.edu.turing.stepProjBookingApp.model.entity.FlightEntity;
-import az.edu.turing.stepProjBookingApp.service.FlightService;
-
-import java.util.List;
-import java.util.Optional;
-
-public class FlightController {
-
-    public FlightService flightService;
-
-    public List<FlightEntity> onlineBoard() {
-        return flightService.getAllFlights();
-    }
-
-    public boolean reservation(FlightDto flightDto) {
-        return flightService.reservation(flightDto);
-    }
-
-    public boolean cancelReservation(long id) {
-        return flightService.cancelReservation(id);
-    }
-
-    public Optional<FlightEntity> getFlightById (long id){
-        return flightService.getFlightById(id);
-    }
-}
+//package az.edu.turing.stepProjBookingApp.controller;
+//
+//import az.edu.turing.stepProjBookingApp.model.dto.FlightDto;
+//import az.edu.turing.stepProjBookingApp.model.entity.FlightEntity;
+//import az.edu.turing.stepProjBookingApp.service.FlightService;
+//
+//import java.util.List;
+//import java.util.Optional;
+//
+//public class FlightController {
+//
+//    public FlightService flightService;
+//
+//    public List<FlightEntity> onlineBoard() {
+//        return flightService.getAllFlights();
+//    }
+//
+//    public boolean reservation(FlightDto flightDto) {
+//        return flightService.reservation(flightDto);
+//    }
+//
+//    public boolean cancelReservation(long id) {
+//        return flightService.cancelReservation(id);
+//    }
+//
+//    public Optional<FlightEntity> getFlightById (long id){
+//        return flightService.getFlightById(id);
+//    }
+//}
