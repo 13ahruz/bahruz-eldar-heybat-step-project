99a602c856c0adb877d0d6cc30e9824a4896649d
Bahruz Shabili <bahruz.dsh@code.edu.az>
2024-05-06 20:27:27 +0400

    FlightServiceImpl test class implemented

---
+++ src/test/az/edu/turing/stepProjBookingApp/service/impl/BookingServiceImplTest.java
@@ -0,0 +1,20 @@
+package az.edu.turing.stepProjBookingApp.service.impl;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BookingServiceImplTest {
+
+    @Test
+    void bookAReservation() {
+    }
+
+    @Test
+    void cancelAReservation() {
+    }
+
+    @Test
+    void getMyReservations() {
+    }
+}
